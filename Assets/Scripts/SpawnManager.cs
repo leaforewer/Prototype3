e9c6a6d7f44bff63885ff215adae143e2406f9c9
Ahmet Erdogan <329188@student.mboutrecht.nl>
2019-12-13 16:52:06 +0100

    Tweede commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstacklePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstackle", startDelay, repeatRate);
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstackle()
+    {
+        if (playerControllerScript.gameOver == false) {
+        Instantiate(obstacklePrefab, spawnPos, obstacklePrefab.transform.rotation); }
+    }
+}
